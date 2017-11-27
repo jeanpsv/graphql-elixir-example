11f42ca960085bc0abe09f1463242cfaaab60f6b
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-11-27 21:52:35 -0200

    Implement all_links query

--- lib/community_web/schema.ex
+++ lib/community_web/schema.ex
@@ -1,9 +1,19 @@
 defmodule CommunityWeb.Schema do
   use Absinthe.Schema
 
-  alias Community.News
+  alias CommunityWeb.NewsResolver
+
+  object :link do
+    field :id, non_null(:id)
+    field :url, non_null(:string)
+    field description, non_null(:string)
+  end
+
+  field :all_links, non_null(list_of(non_null(:link))) do
+    resolve &NewsResolver.all_links/3
+  end
 
   query do
-    
+    field :all_links, non_null(list_of(non_null(:link)))
   end
 end
