4e1b13b6f20b1d5609f2826bb40c64feabe21ce6
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-11-27 22:01:45 -0200

    Expose /graphql and configure GraphiQL

--- lib/community_web/router.ex
+++ lib/community_web/router.ex
@@ -8,4 +8,13 @@ defmodule CommunityWeb.Router do
   scope "/api", CommunityWeb do
     pipe_through :api
   end
+
+  scope "/" do
+    pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: CommunityWeb.Endpoint}
+  end
 end
