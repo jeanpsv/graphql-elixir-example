f3142be1c6fb9285d2ee45ba28a679ae74bbb86a
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-11-30 23:19:43 -0200

    Implement post queries with all_posts

--- lib/community_web/schema/news/post_types.ex
+++ lib/community_web/schema/news/post_types.ex
@@ -7,4 +7,10 @@ defmodule CommunityWeb.Schema.News.PostTypes do
     field :id, non_null(:id)
     field :content, non_null(:string)
   end
+
+  object :post_queries do
+    field :all_posts, non_null(list_of(non_null(:post))) do
+      resolve &Resolvers.News.all_posts/3
+    end
+  end
 end
