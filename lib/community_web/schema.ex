ad3f90871750ecf38cf532ca8b60d96349122bf0
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-11-30 23:19:56 -0200

    Add post types to schema

--- lib/community_web/schema.ex
+++ lib/community_web/schema.ex
@@ -2,9 +2,11 @@ defmodule CommunityWeb.Schema do
   use Absinthe.Schema
 
   import_types CommunityWeb.Schema.News.LinkTypes
+  import_types CommunityWeb.Schema.News.PostTypes
 
   query do
     import_fields :link_queries
+    import_fields :post_queries
   end
 
   mutation do
